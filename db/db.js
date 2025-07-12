e5806d1ee941becd958428c6c779e8be1b75b346
TajulMunandar <tajulmunandar701@gmail.com>
2025-07-12 15:56:35 +0700

    db

--- db/db.js
+++ db/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 
 // Membuat koneksi ke database MySQL
 const db = mysql.createConnection({
-  host: "e-commerce.developerdadakan.com", // Ganti sesuai host MySQL kamu
-  user: "u1585994_e-commerce", // Username MySQL kamu
-  password: "aeibWTe1.j-0", // Password MySQL kamu
-  database: "u1585994_e-commerce", // Nama database yang mau dipakai
+  host: "mysql.railway.internal", // Ganti sesuai host MySQL kamu
+  user: "root", // Username MySQL kamu
+  password: "NNvTtaWBTMyysjPvsGPxLHSORZRxVpGU", // Password MySQL kamu
+  database: "railway", // Nama database yang mau dipakai
 });
 
 // Connect ke database
